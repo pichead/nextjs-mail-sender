4592ef0c4e09ac3a5d7ea43cb697533d19ba6a99
Pichead Maneerat <61868523+pichead@users.noreply.github.com>
2025-03-24 17:56:52 +0700

    Update route.ts

--- app/api/mail/route.ts
+++ app/api/mail/route.ts
@@ -1,8 +1,6 @@
-// app/api/example/route.ts
 import { MAIL } from '@/utils/email';
 import { NextResponse } from 'next/server';
 
-// กำหนดประเภทของข้อมูลที่เราคาดหวังจาก request body
 interface RequestBody {
     email: string;
     text: string;
@@ -12,20 +10,15 @@ interface RequestBody {
 
 export async function POST(request: Request) {
     try {
-        // รับข้อมูลจาก request body
         const body: RequestBody = await request.json();
         const { email, text, emailSender, emailPassword } = body;
 
-        // ตรวจสอบข้อมูล
         if (!email || !text) {
             return NextResponse.json({ error: 'text and email are required' }, { status: 400 });
         }
 
         const sendResult = await MAIL.sendAdsCustom(email, text, emailSender, emailPassword);
-        // ทำบางอย่างกับข้อมูล (เช่น บันทึกลงฐานข้อมูล)
-        // ในตัวอย่างนี้เราจะแค่ส่งข้อมูลกลับไป
-
-        // ส่ง response กลับไปยัง client
+     
         return NextResponse.json(
             { message: 'Data received successfully', data: { sendResult }, statusCode: 200 },
             { status: 200 }
